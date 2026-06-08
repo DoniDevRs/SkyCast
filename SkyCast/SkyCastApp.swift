b8900f325fb6de46c414b8b1fb2642d0e3d48dc8
DoniDevRs <donidevrs@gmail.com>
2026-06-08 18:19:06 -0300

    Initial Commit

---
+++ SkyCast/SkyCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyCastApp.swift
+//  SkyCast
+//
+//  Created by Doni Silva on 08/06/26.
+//
+
+import SwiftUI
+
+@main
+struct SkyCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
